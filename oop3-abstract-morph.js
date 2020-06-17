5e3365b37dffa04b2e7f83e3bafb8c3c960e8aa6
Justin Cheng <ijustin17@gmail.com>
2020-06-17 00:35:31 -0500

    restructured exercise 3 due to function sytax inconsistencies

--- oop3-abstract-morph.js
+++ oop3-abstract-morph.js
@@ -1,6 +1,8 @@
-
 /*
-1. Create a Abstract Creature class that is NOT allowed to be instantiated. Find a way to prevent this class from being instantiated with an instance.  In the Creature class, include two abstract methods.  move(), and act().  These methods are NOT allowed to be invoked from the Abstract class, so throw an error in these methods if they are invoked by any inherited sub-classes.  
+1. Create a Abstract Creature class that is NOT allowed to be instantiated.
+Find a way to prevent this class from being instantiated with an instance.
+In the Creature class, include two abstract methods:  move(), and act().
+These methods are NOT allowed to be invoked from the Abstract class, so throw an error in these methods if they are invoked by any inherited sub-classes.  
 
 Create at least 3 sub-classes that inherit from the Abstract Creature Class.  Example sub-classes are: 
     -Human
@@ -9,7 +11,10 @@ Create at least 3 sub-classes that inherit from the Abstract Creature Class.  Ex
     -Reptile
     -Fish
 
-These sub-classes MUST implement the abstract methods of move() and act(), which are found on the abstract Creature class. In addition to the 2 abstract methods, implement 1 unique method on each sub-class.  Also, each sub-class MUST have at least 2 unique property fields on them.  Example property fields are:
+These sub-classes MUST implement the abstract methods of move() and act(), 
+which are found on the abstract Creature class. 
+In addition to the 2 abstract methods, implement 1 unique method on each sub-class.  
+Also, each sub-class MUST have at least 2 unique property fields on them.  Example property fields are:
     -name
     -weight
     -food
@@ -17,88 +22,91 @@ These sub-classes MUST implement the abstract methods of move() and act(), which
 
 */
 
-    class Creature {
-        //your code here...
-    }
+class Creature {
+  //your code here...
+}
 
-    class Human extends Creature {
-        //your code here...
-    }
-
-
-
+class Human extends Creature {
+  //your code here...
+}
+class Dinosaur extends Creature {
+  //your code here...
+}
+class Bird extends Creature {
+  //your code here...
+}
+class Reptile extends Creature {
+  //your code here...
+}
+class Fish extends Creature {
+  //your code here...
+}
 
 /*
-2. For the following Person class, modify the class methods, so the sub-class methods will successfully fire.  For the Teacher method, a set of methods have already been set up.  For the Student class, it will be up to you to determine how to set up the methods to fire in the Student sub-class.  :
+2. For the following Person class, modify the class methods, so the sub-class methods will successfully fire.  
+For the Teacher method, a set of methods have already been set up.  
+For the Student class, it will be up to you to determine how to set up the methods to fire in the Student sub-class.  :
 */
 
 class Person {
-    name;
+  name;
 
-    eat() {
-        console.log(this.name + " is eating");
-    }
+  eat = () => {
+    console.log(this.name + " is eating");
+  };
 
-    sleep = () => {
-        console.log(this.name + " is sleeping");
-    }
+  sleep = () => {
+    console.log(this.name + " is sleeping");
+  };
 
-    code = function() {
-        console.log(this.name + " is coding");
-    }
+  code = () => {
+    console.log(this.name + " is coding");
+  };
 
-    repeat = function() {
-        console.log(this.name + " is repeating the above steps, yet another time");
-    }
+  repeat = () => {
+    console.log(this.name + " is repeating the above steps, yet another time");
+  };
 
-    explain() {
-        //this function should contain a console.log() explaining what you had to do to get the correct functions to work, and the reasoning behind what you did.
-        console.log("this explain method should contain explain what you had to do to get the correct functions to work, and the reasoning behind what you did.");
-    }
-
+  explain = () => {
+    //this function should contain a console.log() explaining what you had to do to get the correct functions to work, and the reasoning behind what you did.
+    console.log(
+      "this explain method should contain explain what you had to do to get the correct functions to work, and the reasoning behind what you did."
+    );
+  };
 }
-
 
 class Teacher extends Person {
+  //refactor the methods in this Teacher class, so all of these methods override the methods in the Person super class.
 
-    //refactor the methods in this Teacher class, so all of these methods override the methods in the Person super class.  
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
 
-    constructor(name) {
-        super(name);
-        this.name = name;
-    }
+  eat = () => {
+    console.log(this.name + " loves to teach before eating");
+  };
 
-    eat() {
-        console.log(this.name + " loves to teach before eating");
-    }
+  sleep = () => {
+    console.log(this.name + " sleeps after preparing the lesson plan");
+  };
 
-    sleep() {
-        console.log(this.name + " sleeps after preparing the lesson plan");
-    }
+  code = () => {
+    console.log(this.name + " codes first, then teaches it the next day.");
+  };
 
-    code = () => {
-        console.log(this.name + " codes first, then teaches it the next day.");
-    }
-
-    repeat() {
-        console.log(this.name + " teaches, codes, eats, sleeps, and repeats");
-    }
+  repeat = () => {
+    console.log(this.name + " teaches, codes, eats, sleeps, and repeats");
+  };
 }
-
 
 class Student extends Person {
-    //set up your methods in this student class, so all of these methods will override the methods from the super class.
-
-    //eat method should print out - <stduent name> studies, then eats
-
-    //sleep method should print out, <student name> studies coding so much, that they dream about it in their sleep
-
-    //code method should print out, <student name> was first overwhelmed by coding, but kept at it, and now has become a coding guru!
-
-    //repeat method should print out, <student name> keeps on studying, coding, eating, and sleeping, and puts it all on repeat.  
-
+  //set up your methods in this student class, so all of these methods will override the methods from the super class.
+  //eat method should print out - <stduent name> studies, then eats
+  //sleep method should print out, <student name> studies coding so much, that they dream about it in their sleep
+  //code method should print out, <student name> was first overwhelmed by coding, but kept at it, and now has become a coding guru!
+  //repeat method should print out, <student name> keeps on studying, coding, eating, and sleeping, and puts it all on repeat.
 }
-
 
 const teacher = new Teacher("Dr. Teacher");
 
@@ -118,33 +126,29 @@ student.sleep();
 student.code();
 student.repeat();
 
-
-
-
-//****************************************************************************************************************************************************************************************   
+//****************************************************************************************************************************************************************************************
 //Bonus Exercise:
 
 //3. Consider the following class:
 
 class Cook {
+  prepare(food1, food2, food3) {
+    console.log("The cook is cooking " + food1, food2, food3);
+  }
 
-    prepare(food1,food2,food3) {
-        console.log("The cook is cooking " + food1, food2, food3);
-    }
+  prepare = () => {
+    console.log("The cook is cooking");
+  };
 
-    prepare = function() {
-        console.log('The cook is cooking');
-    }
-
-    explain = () => {
-        console.log("what could you do to get the prepare function to print out the food items that are being passed in to the function?  Once you figure it out, Write down your thought process in this explain method.");
-    }
-
+  explain = () => {
+    console.log(
+      "what could you do to get the prepare function to print out the food items that are being passed in to the function?  Once you figure it out, Write down your thought process in this explain method."
+    );
+  };
 }
 
 const cook = new Cook();
 
-cook.prepare("turkey","salami","pizza");
+cook.prepare("turkey", "salami", "pizza");
 
 cook.explain();
-
